565f7f97375b8afdfcedcaf62fdd3f7c9ec9a784
hui <fwu@today36524.com>
2019-01-25 10:27:19 +0800

    fix

---
+++ transaction-manager-service/src/main/scala/com/github/dapeng/tm/scala/service/entity/TGtxStep.scala
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.dapeng.tm.scala.service.entity
+
+import java.sql.Timestamp
+import com.github.dapeng.tm.scala.service.enums.TStatus
+import wangzx.scala_commons.sql.ResultSetMapper
+
+  case class TGtxStep ( 
+ /**  */ 
+id: Int,
+ /** 全局事务id，一般使用服务的会话id(sesstionTid) */ 
+gtxId: Int,
+ /** 子事务序号 */ 
+stepSeq: Int,
+ /** 子事务状态, 1:新建(CREATED);2:成功(SUCCEED);3:失败(FAILED);4:完成(DONE) */ 
+status: TStatus,
+ /** 服务名 */ 
+serviceName: String,
+ /** 服务版本号 */ 
+version: String,
+ /**  */ 
+methodName: String,
+ /**  */ 
+request: Option[Array[Byte]],
+ /**  */ 
+confirmMethodName: Option[String],
+ /**  */ 
+cancelMethodName: Option[String],
+ /**  */ 
+redoTimes: Int,
+ /** 创建时间 */ 
+createdTime: Timestamp,
+ /** 更新时间 */ 
+updatedTime: Timestamp,
+ /** 备注, 每次状态变更都需要追加到remark字段。 */ 
+remark: String,
+) 	
+ 	
+ object TGtxStep { 
+ 	implicit val resultSetMapper: ResultSetMapper[TGtxStep] = ResultSetMapper.material[TGtxStep] 
+ }
---
+++ transaction-manager-service/src/main/scala/com/github/dapeng/tm/scala/service/enums/TStatus.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.dapeng.tm.scala.service.enums
+
+import wangzx.scala_commons.sql.DbEnum
+ import wangzx.scala_commons.sql._ 
+ class TStatus private(val id:Int, val name:String) extends DbEnum {
+ 	 override def toString(): String = "(" + id + "," + name + ")"  
+
+ 	 override def equals(obj: Any): Boolean = { 
+ 			 if (obj == null) false 
+ 			 else if (obj.isInstanceOf[TStatus]) obj.asInstanceOf[TStatus].id == this.id
+ 			 else false 
+ 	 } 
+
+ 	 override def hashCode(): Int = this.id 
+ } 
+
+ object TStatus {
+	 val CREATED = new TStatus(1,"CREATED")
+	 val SUCCEED = new TStatus(2,"SUCCEED")
+	 val FAILED = new TStatus(3,"FAILED")
+	 val DONE = new TStatus(4,"DONE")
+	 def unknown(id: Int) = new TStatus(id, id+"")
+	 def valueOf(id: Int): TStatus = id match {
+ 		 case 1 => CREATED 
+ 		 case 2 => SUCCEED 
+ 		 case 3 => FAILED 
+ 		 case 4 => DONE 
+ 		 case _ => unknown(id) 
+ } 
+ def apply(v: Int) = valueOf(v) 
+ def unapply(v: TStatus): Option[Int] = Some(v.id)
+ implicit object Accessor extends DbEnumJdbcValueAccessor[TStatus](valueOf)
+}
